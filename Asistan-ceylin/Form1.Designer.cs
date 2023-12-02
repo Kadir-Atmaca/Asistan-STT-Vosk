a97f5bae6b6ac2201d6ad9f69f582514fd7c382f
Kadir-Atmaca <152689373+Kadir-Atmaca@users.noreply.github.com>
2023-12-02 16:45:04 +0300

    Add files via upload

---
+++ Asistan-ceylin/Form1.Designer.cs
@@ -0,0 +1,61 @@
+﻿namespace Asistan_ceylin
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(1066, 433);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "label1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1090, 442);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Asistan-ceylin/Form1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Windows.Forms;
+using Vosk;
+using NAudio.Wave;
+
+namespace Asistan_ceylin
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            spk();
+        }
+        private WaveInEvent WaveIn;
+        private Model model;
+        private VoskRecognizer recognizer;
+
+        public void spk()
+        {
+            var waveIn = new WaveInEvent();
+            waveIn.DeviceNumber = 0;
+            waveIn.StartRecording();
+            spk2();
+        }
+        public void spk2()
+        {
+            model = new Model("model");
+            recognizer = new VoskRecognizer(model, 16000.0f);
+            WaveIn = new WaveInEvent();
+            WaveIn.WaveFormat = new WaveFormat(16000, 16, 1);
+            WaveIn.DataAvailable += spk3;
+            WaveIn.StartRecording();
+
+        }
+        private void spk3(object sender, WaveInEventArgs e)
+        {
+            string spk = "";
+            recognizer.AcceptWaveform(e.Buffer, e.BytesRecorded);
+            spk = MTEN.MTEN.mten(recognizer.PartialResult());
+            label1.Invoke(new Action(() =>
+            {
+                label1.Text = spk;
+            }));
+        }
+    }
+}
